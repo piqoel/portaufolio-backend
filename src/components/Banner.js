495471cd9657c8ad01539c4d0278bd52d9db20e0
Taufiiqulhakim <taufiiqulhakimr@gmail.com>
2025-01-02 08:11:33 +0700

    Make the contact session in modal

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { ArrowRightCircle } from "react-bootstrap-icons";
+import { ContactModal } from "./Contact-modal";
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
 import TrackVisibility from "react-on-screen";
@@ -13,49 +13,73 @@ export const Banner = () => {
     const [delta, setDelta] = useState(300 - Math.random() * 100);
     const period = 2000;
 
+    const [showModal, setShowModal] = useState(false);
+
     useEffect(() => {
         let ticker = setInterval(() => {
             tick();
-        }, delta)
+        }, delta);
 
-        return () => { clearInterval(ticker)};
-    }, [text])
+        return () => {
+            clearInterval(ticker);
+        };
+    }, [text]);
 
     const tick = () => {
         let i = loopNum % toRotate.length;
         let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+        let updatedText = isDeleting
+            ? fullText.substring(0, text.length - 1)
+            : fullText.substring(0, text.length + 1);
 
         setText(updatedText);
 
         if (isDeleting) {
-            setDelta(prevDelta => prevDelta /2)
+            setDelta((prevDelta) => prevDelta / 2);
         }
 
         if (!isDeleting && updatedText === fullText) {
             setIsDeleting(true);
             setDelta(period);
-        }   else if (isDeleting && updatedText === '') {
-                setIsDeleting(false);
-                setLoopNum(loopNum + 1);
-                setDelta(500);
-            }
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
         }
+    };
 
     return (
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
-                    <TrackVisibility>
-                    {({isVisible}) =>
-                        <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                            <span className="tagline">Welcome to my Portaufolio</span>
-                            <h1>{`Hi I'm `}<span className="wrap">{text}</span></h1>
-                            <p>A result-oriented GIS and Data Analyst professional with over five years of experience in the government area. Proven track record of leading and managing strategic projects from Head Department of DPMPTSP DKI Jakarta, delivering data with spatial value and innovation in multi-sector. Expertise in end-to-end project planning and coordinating to get efficient and effective results. Seek opportunities to leverage datasets to uncover hidden patterns and solve complex problems with technology.</p>
-                            <button onClick={() => console.log('connect')}>Let's connect</button>
-                        </div>}
-                    </TrackVisibility>
+                        <TrackVisibility>
+                            {({ isVisible }) => (
+                                <div
+                                    className={
+                                        isVisible
+                                            ? "animate__animated animate__fadeIn"
+                                            : ""
+                                    }
+                                >
+                                    <span className="tagline">
+                                        Welcome to my Portfolio
+                                    </span>
+                                    <h1>
+                                        {`Hi I'm `}
+                                        <span className="wrap">{text}</span>
+                                    </h1>
+                                    <p>
+                                        A result-oriented GIS and Data Analyst professional with over five years of experience in the government area. Proven track record of leading and managing strategic projects from Head Department of DPMPTSP DKI Jakarta, delivering data with spatial value and innovation in multi-sector. Expertise in end-to-end project planning and coordinating to get efficient and effective results. Seek opportunities to leverage datasets to uncover hidden patterns and solve complex problems with technology.
+                                    </p>
+                                    <button
+                                        onClick={() => setShowModal(true)}
+                                    >
+                                        Let's connect
+                                    </button>
+                                </div>
+                            )}
+                        </TrackVisibility>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg} alt="Header Img" />
@@ -63,6 +87,11 @@ export const Banner = () => {
                 </Row>
             </Container>
 
+            {/* Use ContactModal */}
+            <ContactModal
+                show={showModal}
+                handleClose={() => setShowModal(false)}
+            />
         </section>
-    )
-}
+    );
+};
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/contact-img.svg";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
@@ -53,18 +52,10 @@ export const Contact = () => {
         <section className="contact" id="connect">
           <Container>
             <Row className="align-items-center">
-              <Col size={12} md={6}>
-                <TrackVisibility>
-                  {({ isVisible }) =>
-                    <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
-                  }
-                </TrackVisibility>
-              </Col>
-              <Col size={12} md={6}>
+              <Col size={12} md={12}>
                 <TrackVisibility>
                   {({ isVisible }) =>
                     <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                    <h2>Get In Touch</h2>
                     <form onSubmit={handleSubmit}>
                       <Row>
                         <Col size={12} sm={6} className="px-1">
@@ -81,7 +72,11 @@ export const Contact = () => {
                         </Col>
                         <Col size={12} className="px-1">
                           <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                          <button type="submit"><span>{buttonText}</span></button>
+                          <div className="button-container">
+                            <button type="submit">
+                              <span>{buttonText}</span>
+                            </button>
+                          </div>
                         </Col>
                         {
                           status.message &&
